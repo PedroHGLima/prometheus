9da1497c3430547c020a777b34824d7114c6e1ae
Natanael Nunes de Moura Junior <natmourajr@gmail.com>
2021-09-07 23:11:34 -0300

    agora vai

--- tools/efficiency/python/EfficiencyTool.py
+++ tools/efficiency/python/EfficiencyTool.py
@@ -96,7 +96,7 @@ class EfficiencyTool( Algorithm ):
 
         # for boosted analysis
         #deltaR_bins = np.arange(0, np.max(eta_bins), step=0.1)
-        deltaR_bins = [0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1,0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.2, 0.22, 0.24, 0.26, 0.28, 0.3, 0.35, 0.40]
+        deltaR_bins = np.array([0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1,0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.2, 0.22, 0.24, 0.26, 0.28, 0.3, 0.35, 0.40])
         sg.addHistogram(TH1F('deltaR','#\Delta R distribution;#\Delta R;Count', len(deltaR_bins)-1, deltaR_bins))
         sg.addHistogram(TH1F('match_deltaR','#\Delta R matched distribution;#\Delta R;Count', len(deltaR_bins)-1, deltaR_bins))
         sg.addHistogram(TProfile('eff_deltaR', "#\epsilon(#\Delta R); #\Delta R ; Efficiency" , len(deltaR_bins)-1, deltaR_bins))
